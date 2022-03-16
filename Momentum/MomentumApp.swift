2d73fee67afe72c721af4dd7804355cab3525352
Ellen Li <x.ellen@gmail.com>
2022-03-16 10:58:26 -0400

    initial commit

---
+++ Momentum/MomentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumApp.swift
+//  Momentum
+//
+//  Created by Ellen Li on 2022-03-16.
+//
+
+import SwiftUI
+
+@main
+struct MomentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
